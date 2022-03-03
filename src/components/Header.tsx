4dc7e0c27c3fcbfb3a4a77f868c10e1b0a375535
Kevin Venâncio <83186522+Kevin414gamer@users.noreply.github.com>
2022-03-03 17:26:12 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,7 @@ export function Header() {
   return (
     <header className="header">
       <div>
-<<<<<<< HEAD
         <img src="../../logo.svg" alt="to.do"/>
-=======
-        <img src="../logo.svg" alt="to.do"/>
->>>>>>> 8feae5ff13b1868a8a2cff0e357d4e29c85fe7fc
       </div>
     </header>
   )
